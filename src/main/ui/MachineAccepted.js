fb96e80b37c0578fb6462bea489b7cff7b1c290c
toxa16 <toxa16@gmail.com>
2019-10-27 23:33:41 +0200

    displaying power status

--- src/main/ui/MachineAccepted.js
+++ src/main/ui/MachineAccepted.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import { PowerStatus } from '../logic/enums';
+
 export default function MachineAccepted({ powerStatus }) {
+  const displayPowerStatus = () => {
+    switch (powerStatus) {
+      case PowerStatus.STARTING: return 'Starting...';
+      case PowerStatus.STARTED: return 'Started';
+      case PowerStatus.STOPPING: return 'Stopping...';
+      default: return 'Stopped';
+    }
+  };
+
+  const getPowerStatusClassName = () => {
+    switch (powerStatus) {
+      case PowerStatus.STARTING: return 'text-warning';
+      case PowerStatus.STARTED: return 'text-success';
+      case PowerStatus.STOPPING: return 'text-warning.';
+      default: return 'text-danger';
+    }
+  };
+
   return (
     <div>
       <span>Power status:</span>
       {' '}
-      <span className="text-danger">Stopped</span>
+      <span className={getPowerStatusClassName()}>
+        { displayPowerStatus() }
+      </span>
     </div>
   );
 }
